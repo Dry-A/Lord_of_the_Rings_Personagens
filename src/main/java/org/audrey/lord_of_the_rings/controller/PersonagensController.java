4ea8f03f1d1d1e88d035450d10ccdd9762f4de9e
Audrey-Dev-Full-Stack <audreydev.br@gmail.com>
2023-12-18 16:27:50 -0300

    :baby_chick: Método findById na Controller

--- src/main/java/org/audrey/lord_of_the_rings/controller/PersonagensController.java
+++ src/main/java/org/audrey/lord_of_the_rings/controller/PersonagensController.java
@@ -3,11 +3,9 @@ package org.audrey.lord_of_the_rings.controller;
 import org.audrey.lord_of_the_rings.model.Personagens;
 import org.audrey.lord_of_the_rings.repository.PersonagensRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +21,12 @@ public class PersonagensController {
     public ResponseEntity<List<Personagens>> getAll(){
         return ResponseEntity.ok(personagensRepository.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Personagens> getById(@PathVariable Long id) {
+        return  personagensRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
 }
