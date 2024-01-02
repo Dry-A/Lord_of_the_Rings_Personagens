1c1661c2b21a677582120ad64c6a2bd4e8f1780b
Audrey-Dev-Full-Stack <audreydev.br@gmail.com>
2024-01-02 20:32:51 -0300

    :tipping_hand_woman: Alteraçao do nome do objeto Repository

--- src/main/java/org/audrey/lord_of_the_rings/controller/PersonagensController.java
+++ src/main/java/org/audrey/lord_of_the_rings/controller/PersonagensController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/personagens")
@@ -16,46 +18,58 @@ import java.util.List;
 public class PersonagensController {
 
     @Autowired
-    private PersonagensRepository personagensRepository;
+    private PersonagensRepository objetoPersonagensRepository;
 
     @GetMapping
     public ResponseEntity<List<Personagens>> getAll(){
-        return ResponseEntity.ok(personagensRepository.findAll());
+        return ResponseEntity.ok(objetoPersonagensRepository.findAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Personagens> getById(@PathVariable Long id) {
-        return  personagensRepository.findById(id)
+        return  objetoPersonagensRepository.findById(id)
                 .map(resposta -> ResponseEntity.ok(resposta))
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @GetMapping("/nome/{nome}")
     public ResponseEntity<List <Personagens> > getByNome(@PathVariable String nome) {
-        return ResponseEntity.ok(personagensRepository.findAllByNomeContainingIgnoreCase(nome));
+        return ResponseEntity.ok(objetoPersonagensRepository.findAllByNomeContainingIgnoreCase(nome));
     }
 
     @GetMapping("/arma/{arma}")
     public ResponseEntity<List <Personagens>> getByArma(@PathVariable String arma) {
-        return ResponseEntity.ok(personagensRepository.findAllByArmaContainingIgnoreCase(arma));
+        return ResponseEntity.ok(objetoPersonagensRepository.findAllByArmaContainingIgnoreCase(arma));
     }
 
     @GetMapping("casa/{casa}")
     public ResponseEntity<List <Personagens>> getByCasa(@PathVariable String casa){
-        return  ResponseEntity.ok(personagensRepository.findAllByCasaContainingIgnoreCase(casa));
+        return  ResponseEntity.ok(objetoPersonagensRepository.findAllByCasaContainingIgnoreCase(casa));
     }
 
     @PostMapping
     public ResponseEntity <Personagens> post(@Valid @RequestBody Personagens meuPersonagem) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(personagensRepository.save(meuPersonagem));
+        return ResponseEntity.status(HttpStatus.CREATED).body(objetoPersonagensRepository.save(meuPersonagem));
     }
 
     @PutMapping
-    public ResponseEntity<Personagens> put(@Valid @RequestBody Personagens meuPersonagem) {
-        return personagensRepository.findById(meuPersonagem.getId())
+    public ResponseEntity <Personagens> put(@Valid @RequestBody Personagens meuPersonagem) {
+        return objetoPersonagensRepository.findById(meuPersonagem.getId())
                 .map(resposta -> ResponseEntity.status(HttpStatus.OK)
-                        .body(personagensRepository.save(meuPersonagem)))
+                        .body(objetoPersonagensRepository.save(meuPersonagem)))
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+
+        Optional<Personagens> meuPersonagem = objetoPersonagensRepository.findById(id);
+
+        if (meuPersonagem.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+            objetoPersonagensRepository.deleteById(id);
     }
 
 }
